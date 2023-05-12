b7eefc50318a0a70a0f9f21e3bd763ca8c0730e6
christiangabriel091 <129456288+christiangabriel091@users.noreply.github.com>
2023-05-12 01:47:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var botao = document.getElementById("botao-alterar-tema");
+botao.addEventListener("click", function() {
+  document.body.classList.toggle("modo-escuro");
+
+  var Sol = document.getElementById("imagem-botao");
+  var Lua = document.getElementById("imagem-botao-2");
+
+  if (document.body.classList.contains("modo-escuro")) {
+    Sol.style.display = "none";
+    Lua.style.display = "block";
+  } else {
+    Sol.style.display = "block";
+    Lua.style.display = "none";
+  }
+});
